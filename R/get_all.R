fc43e162e0ce76ea7d3067cee9c6c0815042388e
Gaurav Sood <gsood07@gmail.com>
2015-09-15 20:44:11 -0400

    get all with curl download fixed

--- R/get_all.R
+++ R/get_all.R
@@ -3,13 +3,14 @@
 #' Get all the data
 #' 
 #' @param job_id ID for the job
+#' @param output_dir output directory
 #' @export
 #' @references \url{https://shreddr.captricity.com/developer/}
 #' @examples \dontrun{
 #' get_all(job_id ="job_id")
 #' }
 
-get_all <- function(job_id ="")
+get_all <- function(job_id ="", output_dir="./")
 {
     app_token = getOption("CaptricityToken")
     if( is.null(app_token)) stop("Please set application token using set_token('app_token').")
@@ -21,9 +22,6 @@ get_all <- function(job_id ="")
     handle_setheaders(h, "Captricity-API-Token" = app_token)
 
     tag_con    <- curl_fetch_memory(paste0("https://shreddr.captricity.com/api/v1/job/", job_id, "/csv/"), handle=h)
-    tag        <- fromJSON(rawToChar(tag_con$content))
-    tag
-    return(invisible(tag))
-    
+    curl_download(tag_con$url, destfile=paste0(output_dir, job_id, ".csv"))    
 }
 
