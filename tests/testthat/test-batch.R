1e0623f78cdb7dc517447be2b98700a723b4963c
Gaurav Sood <gsood07@gmail.com>
2016-05-15 18:35:35 -0400

    added encoded API key for travis testing

--- tests/testthat/test-batch.R
+++ tests/testthat/test-batch.R
@@ -1,9 +1,12 @@
 context("Create Batch")
 
-token <- Sys.getenv('CaptricityToken')
-
 test_that("batch creation happens successfully", {
   skip_on_cran()
+  
+  token_file <- file("captr_key", "r")
+  token <- suppressWarnings(readLines(token_file))
+  close(token_file)
+
   set_token(token)
   batch <- create_batch("new_batch")
   expect_that(batch, is_a("list"))
--- tests/testthat/upload-image.R
+++ tests/testthat/upload-image.R
@@ -1,12 +1,16 @@
 context("Upload Image")
 
-token <- Sys.getenv('CaptricityToken')
-
 test_that("upload image happens successfully", {
   skip_on_cran()
+  
+  token_file <- file("captr_key", "r")
+  token <- suppressWarnings(readLines(token_file))
+  close(token_file)
+
   set_token(token)
+  
   batch <- create_batch("new_batch")
-  path <- system.file("extdata/wisc_ads", package = "captr")
+  path  <- system.file("extdata/wisc_ads", package = "captr")
   files <- dir(path, full.names = TRUE)
   upimage <- lapply(files, upload_image, batch_id = batch$id)
   expect_that(upimage, is_a("list"))
