f010d3ab62a76b1dd5ed4e75482fc948374c4f50
Gaurav Sood <gsood07@gmail.com>
2017-04-14 20:54:33 -0400

    change post to get

--- R/get_template_id.R
+++ R/get_template_id.R
@@ -2,9 +2,10 @@
 #' 
 #' To digitize documents, create a template using the Captricity Web UI at \url{https://shreddr.captricity.com/job/} 
 #' The template tells Captricity which data to get from where in the document.
-#' Get the template id of the template
 #' 
-#' @param \dots Additional arguments passed to \code{\link{captr_POST}}.
+#' Get the template ids of all the documents
+#' 
+#' @param \dots Additional arguments passed to \code{\link{captr_GET}}.
 #' 
 #' @return named list
 #' 
@@ -17,7 +18,7 @@
 
 get_template_id <- function(...) {
 
-  res <- captr_POST(path = "document/", ...)
+  res <- captr_GET(path = "document/", ...)
 
   res
 
